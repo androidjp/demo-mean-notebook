70534ecd76cb713870815dc8e35eee621f3e5d00
Jasper Wu <jasper.wu@cargosmart.com>
2017-11-17 17:51:09 +0800

    add some 注释

--- DEMO_NG_PRS_NOTEBOOK/tasks/config/webpack.dev.config.js
+++ DEMO_NG_PRS_NOTEBOOK/tasks/config/webpack.dev.config.js
@@ -35,5 +35,14 @@ var webpackDevConfig = merge(webpackBaseConfig,{
     },
     plugins: [
         ///TODO : add plugins
+        /// 声明一个前端构建时的环境变量
+        // 这种变量的作用：
+        // 1. 前端文件中 可见：通过 `process.env.NODE_ENV` 得到
+        // 2. 浏览器中无法得到
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: ''
+            }
+        })
     ]
 })
