ea0088f68fd092bff12cdb85c573a6390121108c
androidjp <1049578026@qq.com>
2017-11-20 20:15:11 +0800

    delete old PRS

--- NG_PRS_NOTEBOOK/gulpfile.js
+++
@@ -1,5 +0,0 @@
-/** Created by CUIJA on 05-19-2017.*/
-
-require('./tasks/clean');
-require('./tasks/build');
-require('./tasks/serve');
--- NG_PRS_NOTEBOOK/src/app/common/components/navbar/navbar.js
+++
@@ -1,6 +0,0 @@
-/* Created by Aquariuslt on 5/20/17.*/
-
-module.exports = {
-  template: require('./template/flat-navbar.html')
-};
-
